56e24903c0087aeb9cdb552909e41ea4e21b2fc7
Einar Ingebrigtsen <einari@me.com>
2023-05-02 19:21:04 +0200

    Initial move

---
+++ Source/DotNET/Reactive/ObservableCollectionCleared.cs
@@ -0,0 +1,9 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Aksio.Reactive;
+
+/// <summary>
+/// Delegate representing the action of an <see cref="IObservableCollection{TItem}"/> being cleared.
+/// </summary>
+public delegate void ObservableCollectionCleared();
