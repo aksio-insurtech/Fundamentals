368a5caaa1663338db29bc589b6146a9e13fa86c
Einar Ingebrigtsen <einari@me.com>
2023-05-22 15:39:10 +0200

    Moving the forgotten specs from Cratis

---
+++ Specifications/Reflection/for_TypeConstructorExtensions/TypeWithDefaultAndNonDefaultConstructor.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Aksio.Reflection.for_TypeExtensions;
+
+public class TypeWithDefaultAndNonDefaultConstructor
+{
+    public TypeWithDefaultAndNonDefaultConstructor()
+    {
+    }
+
+    public TypeWithDefaultAndNonDefaultConstructor(string something)
+    {
+    }
+}
---
+++ Specifications/Types/for_ContractToImplementorsMap/given/an_empty_map.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Aksio Insurtech. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Aksio.Types.for_ContractToImplementorsMap.given;
+
+public class an_empty_map : Specification
+{
+    protected ContractToImplementorsMap map;
+
+    void Establish() => map = new();
+}
