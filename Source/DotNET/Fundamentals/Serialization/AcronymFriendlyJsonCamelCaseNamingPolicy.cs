2b27da855dff981bf35b602106d12dbae7a940ba
Michael Smith <michael.smith@aksio.no>
2023-12-15 13:19:40 +0100

    Fix style and compiler issues

--- Source/DotNET/Fundamentals/Serialization/AcronymFriendlyJsonCamelCaseNamingPolicy.cs
+++ Source/DotNET/Fundamentals/Serialization/AcronymFriendlyJsonCamelCaseNamingPolicy.cs
@@ -6,9 +6,16 @@ using Aksio.Strings;
 
 namespace Aksio.Serialization;
 
-public class AcronymFriendlyJsonCamelCaseNamingPolicy:  JsonNamingPolicy
+/// <summary>
+/// A <see cref="JsonNamingPolicy"/> that converts names to camel case, but also takes acronyms into consideration.
+/// </summary>
+public class AcronymFriendlyJsonCamelCaseNamingPolicy : JsonNamingPolicy
 {
+    /// <summary>
+    /// A singleton instance of the <see cref="AcronymFriendlyJsonCamelCaseNamingPolicy"/>.
+    /// </summary>
     public static readonly JsonNamingPolicy Instance = new AcronymFriendlyJsonCamelCaseNamingPolicy();
+
     /// <inheritdoc/>
     public override string ConvertName(string name) => name.ToCamelCase();
 }
